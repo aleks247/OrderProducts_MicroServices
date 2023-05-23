5c06430e7718e265e4510409f76fcbad02e2e8b7
Aleks247 <aleks247@abv.bg>
2023-05-23 15:33:08 +0300

    Adding Inventory Microservice

---
+++ InventoryMicroservice/src/test/java/com/MicroServices/Inventory/InventoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroServices.Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
